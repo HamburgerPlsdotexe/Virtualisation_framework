1cf1a3e5a9426d21a74ab2bad5dd339109e920a2
Çaglar <caglar_G@live.nl>
2022-05-11 17:54:23 +0200

    NameGenerator + Radzen - Blazority

--- Program.cs
+++ Program.cs
@@ -15,7 +15,17 @@ namespace VFrameworkBlazor
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            //VFrameworkBlazor.Utility.NameGenerator test = new("CAS", "SYS", "we", "dev", "w", "l", "web", "sql", "auth", "backlog");
+            //Dictionary<string, string> dict = test.getDict();
+            //Dictionary<string, string>.KeyCollection keys = dict.Keys;
+            //Dictionary<string, string>.ValueCollection vals = dict.Values;
+
+            //foreach (string key in keys)
+            //{
+            //    Console.WriteLine("Key: " + key + " value: " +  dict[key]);
+            //}
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
