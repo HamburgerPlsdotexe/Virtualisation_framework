740f5584fcf500e57c682a197cb7426ed699c300
Çağlar Gül <cgu@netcompany.com>
2022-05-13 10:22:33 +0200

    dddd

--- obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/App.razor.g.cs
@@ -13,84 +13,84 @@ namespace VFrameworkBlazor
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 1 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 2 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 3 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 4 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 5 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 6 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 7 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 8 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 9 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using VFrameworkBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 10 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using VFrameworkBlazor.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 11 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using HashiCorp.Cdktf;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\cagla\source\repos\VFrameworkBlazor\VFrameworkBlazor\_Imports.razor"
+#line 12 "C:\Users\CGU\source\repos\Virtualisation_framework\_Imports.razor"
 using HashiCorp.Cdktf.Providers.Azurerm;
 
 #line default
